8f5e0f8ac41508139237aa2721c0599dc9f5fc07
meng.xu <xum1281517532@163.com>
2019-03-01 16:42:23 +0800

    add spring-cloud-bus, need double check it

--- eureka-client/src/main/java/com/xum/eurekaclient/controller/TestOneController.java
+++ eureka-client/src/main/java/com/xum/eurekaclient/controller/TestOneController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
