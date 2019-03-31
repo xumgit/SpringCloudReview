bc2f5dbf79def73792ddda23a3e4b22ed5839c82
xum <xum1281517532@163.com>
2019-03-31 17:08:18 +0800

    change content
    change content

--- zipkin-server/src/main/java/com/xum/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/xum/zipkinserver/ZipkinServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import zipkin2.storage.mysql.v1.MySQLStorage;
