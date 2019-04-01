83d5b61c53f5977aca18a316cde062c32e632f75
meng.xu <xum1281517532@163.com>
2019-04-01 08:54:59 +0800

    regit sprincloudreview project

---
+++ ribbon-consumer/src/main/java/com/xum/ribbonconsumer/RibbonConsumerApplication.java
@@ -0,0 +1,28 @@
+package com.xum.ribbonconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RibbonConsumerApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate () {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonConsumerApplication.class, args);
+    }
+
+}
